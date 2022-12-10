2a7fe52c3ec52d09f6fe1fbc8a971a07fb6c4e06
Dhivyajoel <chat2joe86@gmail.com>
2022-12-10 13:07:46 +0530

    Rest Assured

---
+++ src/test/java/restAPI/GetRequest.java
@@ -0,0 +1,30 @@
+package restAPI;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+
+public class GetRequest {
+	
+    
+	@Test
+public void test() {
+		
+	//RestAssured.baseURI= "http://localhost:3000";
+		
+		RestAssured.baseURI = "http://localhost:3000/";
+		RequestSpecification request = RestAssured.given();
+		
+		Response response = request.get("employees");
+		
+		System.out.println(response.getBody().asString());
+		
+		
+		
+	}
+	
+}
+	
